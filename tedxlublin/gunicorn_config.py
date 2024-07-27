9797d29671163c954b4c1339bf108f5dc0d77276
artpods56 <artpods56@gmail.com>
2024-07-27 19:25:11 +0200

    chore: updated nginx and production configuration and added certbot for SSL certificate management

---
+++ tedxlublin/gunicorn_config.py
@@ -0,0 +1,9 @@
+bind = "0.0.0.0:8000"
+module = "tedxlublin.wsgi:application"
+
+workers = 4  # Adjust based on your server's resources
+worker_connections = 1000
+threads = 4
+
+certfile = "/etc/letsencrypt/live/tedxlublin.rocks/fullchain.pem"
+keyfile = "/etc/letsencrypt/live/tedxlublin.rocks/privkey.pem"
