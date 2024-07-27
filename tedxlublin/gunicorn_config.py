35d057a0faf4570e5e20c09bbd7937a95e721acc
artpods56 <artpods56@gmail.com>
2024-07-27 19:16:58 +0000

    milestone: nginx properly handles requests

--- tedxlublin/gunicorn_config.py
+++ tedxlublin/gunicorn_config.py
@@ -5,5 +5,5 @@ workers = 4  # Adjust based on your server's resources
 worker_connections = 1000
 threads = 4
 
-certfile = "/etc/letsencrypt/live/tedxlublin.rocks/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/tedxlublin.rocks/privkey.pem"
+#certfile = "/etc/letsencrypt/live/tedxlublin.rocks/fullchain.pem"
+#keyfile = "/etc/letsencrypt/live/tedxlublin.rocks/privkey.pem"
