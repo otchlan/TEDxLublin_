74d0d99597bd7a91e6a9e59fea74324fa028ad7c
artpods56 <artpods56@gmail.com>
2024-07-28 13:27:54 +0200

    chore: made changes to nginx configuration to fix staticfiles serving

--- tedxlublin/gunicorn_config.py
+++ tedxlublin/gunicorn_config.py
@@ -4,6 +4,5 @@ module = "tedxlublin.wsgi:application"
 workers = 4  # Adjust based on your server's resources
 worker_connections = 1000
 threads = 4
-
-certfile = "/etc/letsencrypt/live/tedxlublin.rocks/fullchain.pem"
-keyfile = "/etc/letsencrypt/live/tedxlublin.rocks/privkey.pem"
+#certfile = "/etc/letsencrypt/live/tedxlublin.rocks/fullchain.pem"
+#keyfile = "/etc/letsencrypt/live/tedxlublin.rocks/privkey.pem"
