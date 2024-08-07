72b10107373e12b30fc2ae41ff7dfcd095ab2987
artpods56 <artpods56@gmail.com>
2024-08-08 01:32:09 +0200

    chore: updated environment variables and configurations for production deployment, added postgresql database

--- tedxlublin/tedxlublin/settings.py
+++ tedxlublin/tedxlublin/settings.py
@@ -83,17 +83,19 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tedxlublin.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('POSTGRES_DB'),
+        'USER': env('POSTGRES_USER'),
+        'PASSWORD': env('POSTGRES_PASSWORD'),
+        'HOST': env('POSTGRES_HOST'),
+        'PORT': env('POSTGRES_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
